b27c946de11d764cb65d8b31e33e38a9373e74b1
Michael Luis <mrl@nottres.com>
2015-10-23 18:55:54 +0800

    Code prettify & wp funcs ready

--- my-pjax/mypjax.main.js
+++ my-pjax/mypjax.main.js
@@ -2,76 +2,66 @@
  * Made by Michael on 17th,Oct. 2015
  */
 
-var container="#pjax",
-	fragment="#pjax",
-	timeout=3000;
-(function($){
-	/* Main pjax entry */
-	$(document).pjax("a[class!='post-edit-link'][class!='comment-edit-link']",container,{
-		timeout:timeout,
-		fragment:fragment,
-		scrollTo:false
+var container = "#pjax",
+	fragment = "#pjax",
+	timeout = 3000;
+(function($) {
+	$(document).pjax("a[class!='post-edit-link'][class!='comment-edit-link']", container, {
+		timeout: timeout,
+		fragment: fragment,
+		scrollTo: false
 	});
-	/* Handles comments fired for pjax */
-	function mypj(){
-		/* Search for forms */
-		$("form").each(function(){
-			var form=$(this);
-			/* If comment form found */
-			if(form.hasClass('comment-form')){
-				/* Attach event for comment form */
-				form.on('submit',function(){
-					var url=form.attr('action'),data='';
-					/* Parse all params */
-					form.find('input,textarea').each(function(){
-						var that=$(this);
-						data+="&"+that.attr("name")+"="+encodeURIComponent(that.val());
+
+	function pjaxinit() {
+		$("form").each(function() {
+			var form = $(this);
+			if (form.hasClass('comment-form')) {
+				form.on('submit', function() {
+					var url = form.attr('action'),
+						data = '';
+					form.find('input,textarea').each(function() {
+						var that = $(this);
+						data += "&" + that.attr("name") + "=" + encodeURIComponent(that.val());
 					});
-					/* Send ajax request */
-					$.post(url,data,function(){
+					NProgress.start();
+					$.post(url, data, function() {
 						$.pjax({
-							url:window.location.href,
-							container:container,
-							fragment:fragment,
-							timeout:timeout
+							url: window.location.href,
+							container: container,
+							fragment: fragment,
+							timeout: timeout
 						});
 					});
-					/* If error do nothing */
+					NProgress.done();
 					return false;
 				});
-				/* If search form found */
-			}else if(form.hasClass('search-form')){
-				var form=$(this);
-				form.on('submit',function(){
-					/* Get arguments */
-					var url=form.attr('action'),
-						word=form.find('.search-field').val();
-					/* Load pjax */
+			} else if (form.hasClass('search-form')) {
+				var form = $(this);
+				form.on('submit', function() {
+					var url = form.attr('action'),
+						word = form.find('.search-field').val();
 					$.pjax({
-						url:url+"?s="+encodeURIComponent(word),
-						container:container,
-						fragment:container,
-						timeout:timeout
+						url: url + "?s=" + encodeURIComponent(word),
+						container: container,
+						fragment: container,
+						timeout: timeout
 					});
-					/* Do nothing when error */
 					return false;
 				});
 			}
 		});
 	}
-	$(document).on("pjax:send",function(){
-		/* Start progress bar */
+	$(document).on("pjax:send", function() {
 		NProgress.start();
 	});
-	$(document).on("pjax:complete",function(){
+	$(document).on("pjax:complete", function() {
 		NProgress.done();
-		/* Attach events */
-		mypj();
+		pjaxinit();
 	});
-	$(document).ready(function(){
-		/* Designed for Internet Explorer */
-		$.ajaxSetup({cache:false});
-		/* Initialize */
-		mypj();
+	$(document).ready(function() {
+		$.ajaxSetup({
+			cache: false
+		});
+		pjaxinit();
 	});
 })(jQuery);
--- my_pjax.php
+++ my_pjax.php
@@ -17,18 +17,13 @@
  * License:		GPL
  */
 
-function mp_activate(){
-	$mp_js=plugin_dir_url(__FILE__).'mypjax.main.js';
-	echo "<script type='text/javascript' src='//cdn.bootcss.com/jquery.pjax/1.9.6/jquery.pjax.min.js'></script>
-<script type='text/javascript' src='$mp_js'></script>
-";
+function mp_activate()
+{
+    $mp_js = plugin_dir_url(__FILE__) . 'mypjax.main.js';
+    wp_enqueue_script('jquery-pjax', '//cdn.bootcss.com/jquery.pjax/1.9.6/jquery.pjax.min.js', array('jquery'), false, true);
+    wp_enqueue_script('mypjax-main', $mp_js, array(), false, true);
+    wp_enqueue_style('nprogress', 'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css');
+    wp_enqueue_script('nprogress', 'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js', array('jquery'));
 }
-add_action('wp_footer','mp_activate');
 
-function mp_nprogress(){
-	wp_enqueue_style('nprogress','https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css');
-	wp_enqueue_script('nprogress','https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',array('jquery'));
-}
-add_action('wp_head','mp_nprogress');
-
-?>
+add_action('wp_head', 'mp_activate');
